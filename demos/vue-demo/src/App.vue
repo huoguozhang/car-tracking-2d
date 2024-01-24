ab68c35554b74af63fbb2acc49547c65ffdc283a
zlj <zlj@qq.com>
2024-01-24 12:30:58 +0800

    feat: 引入vue deom

---
+++ demos/vue-demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TrackComp></TrackComp>
+</template>
+
+<script>
+import TrackComp from './TrackComp'
+
+export default {
+  name: 'App',
+  components: {
+    TrackComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
